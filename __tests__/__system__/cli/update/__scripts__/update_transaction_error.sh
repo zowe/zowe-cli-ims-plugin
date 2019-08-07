c54863581d1f1a8ce7d74a494ab669c166090103
PeteSwauger <37373824+PeteSwauger@users.noreply.github.com>
2019-08-07 09:49:57 -0400

    Added comment to example_properties.yaml
    Signed-off-by: PeteSwauger <37373824+PeteSwauger@users.noreply.github.com>

--- __tests__/__system__/cli/update/__scripts__/update_transaction_error.sh
+++ __tests__/__system__/cli/update/__scripts__/update_transaction_error.sh
@@ -8,8 +8,6 @@ fp=$4
 route=$5
 segno=$6
 transtat=$7
-spasz=$8
-spatrunc=$9
 
-zowe ims update transaction "$1" --aocmd $2 --conv $3 --fp $4 --rte $5 --segno $6 --transtat $7 --spasz $8 --spatrunc $9
+zowe ims update transaction "$1" --aocmd $2 --conv $3 --fp $4 --rte $5 --segno $6 --transtat $7
 
